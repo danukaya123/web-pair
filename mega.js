32a0ae9423c1f62ad7da7c4074ca66c2a1adfba0
Danuka <danukaandhimasha@gmail.com>
2025-04-22 19:07:51 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-    email: 'haniwo5360@opposir.com',
-    password: 'Anuhas2010',
+    email: 'dinithihansika21865@gmail.com',
+    password: 'danuka$123',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
